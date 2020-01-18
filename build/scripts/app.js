f695868a1294bbc4ccc8115aff6370c321d03e86
amrhamdybbn <amr.hamdy@businessboomers.net>
2020-01-19 01:58:32 +0200

    first

---
+++ build/scripts/app.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){$(".icon_head").click(function(){$(".icon_Allcontent").toggleClass("showcontent")})});
---
+++ src/scripts/app.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+    $(".icon_head").click(function(){
+        $(".icon_Allcontent").toggleClass("showcontent")
+    })
+});
